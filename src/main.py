620558496eb6c0872304bc3eb2ae81faeb3ee69b
abroniewski <abroniewski@gmail.com>
2022-05-18 09:04:52 +0200

    initial commit

---
+++ src/main.py
@@ -0,0 +1,21 @@
+#TODO: complete TBOX creation with all properties from lab scope
+
+#TODO: import RDFlib
+
+#TODO: create graph using existing turtle doc
+
+#TODO: create script to reformat existing CSV files into turtle format, export to turtle doc
+
+#TODO: complete ABOX creation so that files can be imported to GraphDB
+
+#TODO: create code that links ABOX and TBOX
+
+#TODO: import turtle doc (ABOX and TBOX) into graphDB
+
+#TODO: complete queries
+
+#TODO: write SPARQL queries
+# 1. Find all Authors.
+# 2. Find all properties whose domain is Author.
+# 3. Find all properties whose domain is either Conference or Journal.
+# 4. Find all the papers written by a given author that where published in database confer- ences.
