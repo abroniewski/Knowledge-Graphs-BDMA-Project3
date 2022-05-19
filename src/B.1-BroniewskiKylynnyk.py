5a7a1352573d6a2ee0e328a41e9869be4f1466a4
abroniewski <abroniewski@gmail.com>
2022-05-19 19:48:25 +0200

    added labels and comments. still need to fix up some properties that have multiple domains

--- src/B.1-BroniewskiKylynnyk.py
+++ src/B.1-BroniewskiKylynnyk.py
@@ -1,7 +1,8 @@
 # TBOX definition
 from rdflib import Graph
-from rdflib.namespace import RDFS, RDF
-from rdflib import URIRef
+from rdflib.namespace import RDFS, RDF, XSD
+from rdflib import Namespace
+from rdflib import Literal
 
 
 def create_TBOX():
@@ -11,144 +12,170 @@ def create_TBOX():
     in RDFlib capabilities.
     :return: None
     """
+    # Namespace means that we can just use "KG" as an "alias" for the URI
+    KG = Namespace("http://KG-SDM-Lab3.org/")
+    # binding is what adds the @prefix to the turtle file to make it easier to read in the output.
+    graph.bind("kg", KG)
 
     # Creating superclasses
-    Paper = URIRef(f'{baseURI}/Paper')
-    graph.add((Paper, RDF.type, RDFS.Class))
+    graph.add((KG.Paper, RDF.type, RDFS.Class))
+    graph.add((KG.Paper, RDFS.label, Literal("Paper")))
+    graph.add((KG.Paper, RDFS.comment, Literal("To know if a paper is published or not, complete a query on the "
+                                               "Decision of a paper. If Decision is True, the paper is published.")))
 
-    SubjectArea = URIRef(f'{baseURI}/SubjectArea')
-    graph.add((SubjectArea, RDF.type, RDFS.Class))
+    graph.add((KG.SubjectArea, RDF.type, RDFS.Class))
+    graph.add((KG.SubjectArea, RDFS.label, Literal("SubjectArea")))
 
-    Submission = URIRef(f'{baseURI}/Submission')
-    graph.add((Submission, RDF.type, RDFS.Class))
+    graph.add((KG.Submission, RDF.type, RDFS.Class))
+    graph.add((KG.Submission, RDFS.label, Literal("Submission")))
 
-    Keyword = URIRef(f'{baseURI}/Keyword')
-    graph.add((Keyword, RDF.type, RDFS.Class))
+    graph.add((KG.Venue, RDF.type, RDFS.Class))
+    graph.add((KG.Venue, RDFS.label, Literal("Venue")))
 
-    Venue = URIRef(f'{baseURI}/Venue')
-    graph.add((Venue, RDF.type, RDFS.Class))
+    graph.add((KG.Author, RDF.type, RDFS.Class))
+    graph.add((KG.Author, RDFS.label, Literal("Author")))
 
-    Author = URIRef(f'{baseURI}/Author')
-    graph.add((Author, RDF.type, RDFS.Class))
+    graph.add((KG.Reviewer, RDF.type, RDFS.Class))
+    graph.add((KG.Reviewer, RDFS.label, Literal("Reviewer")))
 
-    Organization = URIRef(f'{baseURI}/Organization')
-    graph.add((Organization, RDF.type, RDFS.Class))
+    graph.add((KG.Chair, RDF.type, RDFS.Class))
+    graph.add((KG.Chair, RDFS.label, Literal("Chair")))
 
-    Review = URIRef(f'{baseURI}/Review')
-    graph.add((Review, RDF.type, RDFS.Class))
+    graph.add((KG.Editor, RDF.type, RDFS.Class))
+    graph.add((KG.Editor, RDFS.label, Literal("Editor")))
 
-    Feedback = URIRef(f'{baseURI}/Feedback')
-    graph.add((Feedback, RDF.type, RDFS.Class))
+    graph.add((KG.Proceeding, RDF.type, RDFS.Class))
+    graph.add((KG.Proceeding, RDFS.label, Literal("Proceeding")))
 
-    Decision = URIRef(f'{baseURI}/Decision')
-    graph.add((Decision, RDF.type, RDFS.Class))
-
-    Year = URIRef(f'{baseURI}/Year')
-    graph.add((Year, RDF.type, RDFS.Class))
-
-    Chair = URIRef(f'{baseURI}/Chair')
-    graph.add((Chair, RDF.type, RDFS.Class))
-
-    Editor = URIRef(f'{baseURI}/Editor')
-    graph.add((Editor, RDF.type, RDFS.Class))
-
+    graph.add((KG.Volume, RDF.type, RDFS.Class))
+    graph.add((KG.Volume, RDFS.label, Literal("Volume")))
 
     #Creating subclasses
-    ShortPaper = URIRef(f'{baseURI}/ShortPaper')
-    graph.add((ShortPaper, RDFS.subClassOf, Paper))
+    graph.add((KG.ShortPaper, RDFS.subClassOf, KG.Paper))
+    graph.add((KG.ShortPaper, RDFS.label, Literal("ShortPaper")))
 
-    FullPaper = URIRef(f'{baseURI}/FullPaper')
-    graph.add((FullPaper, RDFS.subClassOf, Paper))
+    graph.add((KG.FullPaper, RDFS.subClassOf, KG.Paper))
+    graph.add((KG.FullPaper, RDFS.label, Literal("FullPaper")))
 
-    DemoPaper = URIRef(f'{baseURI}/DemoPaper')
-    graph.add((DemoPaper, RDFS.subClassOf, Paper))
+    graph.add((KG.DemoPaper, RDFS.subClassOf, KG.Paper))
+    graph.add((KG.DemoPaper, RDFS.label, Literal("DemoPaper")))
 
-    Poster = URIRef(f'{baseURI}/Poster')
-    graph.add((Poster, RDFS.subClassOf, Paper))
+    graph.add((KG.Poster, RDFS.subClassOf, KG.Paper))
+    graph.add((KG.Poster, RDFS.label, Literal("Poster")))
 
-    Journal = URIRef(f'{baseURI}/Journal')
-    graph.add((Journal, RDFS.subClassOf, Venue))
+    graph.add((KG.Journal, RDFS.subClassOf, KG.Venue))
+    graph.add((KG.Journal, RDFS.label, Literal("Journal")))
 
-    Conference = URIRef(f'{baseURI}/Conference')
-    graph.add((Conference, RDFS.subClassOf, Venue))
+    graph.add((KG.Conference, RDFS.subClassOf, KG.Venue))
+    graph.add((KG.Conference, RDFS.label, Literal("Conference")))
 
-    Workshop = URIRef(f'{baseURI}/Workshop')
-    graph.add((Workshop, RDFS.subClassOf, Conference))
+    graph.add((KG.Workshop, RDFS.subClassOf, KG.Conference))
+    graph.add((KG.Workshop, RDFS.label, Literal("Workshop")))
 
-    Symposium = URIRef(f'{baseURI}/Symposium')
-    graph.add((Symposium, RDFS.subClassOf, Conference))
+    graph.add((KG.Symposium, RDFS.subClassOf, KG.Conference))
+    graph.add((KG.Symposium, RDFS.label, Literal("Symposium")))
 
-    ExpertGroup = URIRef(f'{baseURI}/ExpertGroup')
-    graph.add((ExpertGroup, RDFS.subClassOf, Conference))
+    graph.add((KG.ExpertGroup, RDFS.subClassOf, KG.Conference))
+    graph.add((KG.ExpertGroup, RDFS.label, Literal("ExpertGroup")))
 
-    RegularConference = URIRef(f'{baseURI}/RegularConference')
-    graph.add((RegularConference, RDFS.subClassOf, Conference))
+    graph.add((KG.RegularConference, RDFS.subClassOf, KG.Conference))
+    graph.add((KG.RegularConference, RDFS.label, Literal("RegularConference")))
 
 
     #Creating properties
-    relatedTo = URIRef(f'{baseURI}/relatedTo')
-    graph.add((relatedTo, RDFS.domain, Paper))
-    graph.add((relatedTo, RDFS.range, SubjectArea))
+    #TODO: Should the "duplicate" properties be sub-properties instead? Because of the queries we are doing,
+    # there does not look to be value in doing this.
+    graph.add((KG.relatedTo, RDFS.domain, KG.Paper))
+    graph.add((KG.relatedTo, RDFS.domain, KG.Conference))
+    graph.add((KG.relatedTo, RDFS.domain, KG.Journal))
+    graph.add((KG.relatedTo, RDFS.range, KG.SubjectArea))
+    graph.add((KG.relatedTo, RDFS.label, Literal("relatedTo")))
 
-    citedBy = URIRef(f'{baseURI}/citedBy')
-    graph.add((citedBy, RDFS.domain, Paper))
-    graph.add((citedBy, RDFS.range, Paper))
+    graph.add((KG.title, RDFS.domain, KG.Paper))
+    graph.add((KG.title, RDFS.domain, KG.Journal))
+    graph.add((KG.title, RDFS.domain, KG.Conference))
 
-    submitted = URIRef(f'{baseURI}/submitted')
-    graph.add((submitted, RDFS.domain, Paper))
-    graph.add((submitted, RDFS.range, Submission))
+    graph.add((KG.title, RDF.type, RDF.Property))
+    graph.add((KG.title, RDFS.label, Literal("title")))
+    graph.add((KG.title, RDFS.range, XSD.string))
 
-    contains = URIRef(f'{baseURI}/contains')
-    graph.add((contains, RDFS.domain, Paper))
-    graph.add((contains, RDFS.range, Keyword))
+    graph.add((KG.submitted, RDFS.domain, KG.Paper))
+    graph.add((KG.submitted, RDFS.range, KG.Submission))
+    graph.add((KG.submitted, RDFS.label, Literal("submitted")))
 
-    participatedIn = URIRef(f'{baseURI}/participatedIn')
-    graph.add((participatedIn, RDFS.domain, Author))
-    graph.add((participatedIn, RDFS.range, Paper))
+    graph.add((KG.contains, RDFS.domain, KG.Paper))
+    graph.add((KG.contains, RDFS.range, KG.Keyword))
+    graph.add((KG.contains, RDFS.label, Literal("contains")))
 
-    relatedTo = URIRef(f'{baseURI}/relatedTo')
-    graph.add((relatedTo, RDFS.domain, Conference))
-    graph.add((relatedTo, RDFS.range, SubjectArea))
+    graph.add((KG.participatedIn, RDFS.domain, KG.Author))
+    graph.add((KG.participatedIn, RDFS.range, KG.Paper))
+    graph.add((KG.participatedIn, RDFS.label, Literal("participatedIn")))
 
-    relatedTo = URIRef(f'{baseURI}/relatedTo')
-    graph.add((relatedTo, RDFS.domain, Journal))
-    graph.add((relatedTo, RDFS.range, SubjectArea))
+    #TODO: If I have a repeating property like this, does it suggest I should have a super classe (Person?). Should I
+    # be setting the domain for name like this at the schematic level, or should this be done when instantiating?
+    graph.add((KG.name, RDFS.domain, KG.Author))
+    graph.add((KG.name, RDFS.domain, KG.Reviewer))
+    graph.add((KG.name, RDFS.domain, KG.Editor))
+    graph.add((KG.name, RDFS.domain, KG.Chair))
 
-    publishedIn = URIRef(f'{baseURI}/publishedIn')
-    graph.add((publishedIn, RDFS.domain, Submission))
-    graph.add((publishedIn, RDFS.range, Venue))
+    graph.add((KG.name, RDF.type, RDF.Property))
+    graph.add((KG.name, RDFS.label, Literal("name")))
+    graph.add((KG.name, RDFS.range, XSD.string))
 
-    reviewed = URIRef(f'{baseURI}/reviewed')
-    graph.add((reviewed, RDFS.domain, Author))
-    graph.add((reviewed, RDFS.range, Submission))
+    graph.add((KG.publishedIn, RDFS.domain, KG.Submission))
+    graph.add((KG.publishedIn, RDFS.range, KG.Venue))
+    graph.add((KG.publishedIn, RDFS.label, Literal("publishedIn")))
 
-    affiliatedTo = URIRef(f'{baseURI}/relatedTo')
-    graph.add((affiliatedTo, RDFS.domain, Author))
-    graph.add((affiliatedTo, RDFS.range, Organization))
+    graph.add((KG.published, RDFS.domain, KG.Conference))
+    graph.add((KG.published, RDFS.range, KG.Proceeding))
+    graph.add((KG.published, RDFS.domain, KG.Journal))
+    graph.add((KG.published, RDFS.range, KG.Volume))
+    graph.add((KG.published, RDFS.label, Literal("published")))
 
-    partOf = URIRef(f'{baseURI}/partOf')
-    graph.add((partOf, RDFS.domain, Submission))
-    graph.add((partOf, RDFS.range, Review))
+    graph.add((KG.handledBy, RDFS.domain, KG.Conference))
+    graph.add((KG.handledBy, RDFS.range, KG.Chair))
+    graph.add((KG.handledBy, RDFS.domain, KG.Journal))
+    graph.add((KG.handledBy, RDFS.range, KG.Editor))
+    graph.add((KG.handledBy, RDFS.label, Literal("handledBy")))
 
-    commented = URIRef(f'{baseURI}/commented')
-    graph.add((commented, RDFS.domain, Review))
-    graph.add((commented, RDFS.range, Feedback))
+    graph.add((KG.assigned, RDFS.domain, KG.Editor))
+    graph.add((KG.assigned, RDFS.domain, KG.Chair))
+    graph.add((KG.assigned, RDFS.range, KG.Reviewer))
+    graph.add((KG.assigned, RDFS.label, Literal("assigned")))
+    graph.add((KG.assigned, RDFS.comment, Literal("Each class assigning a reviewer should assign at least 2 "
+                                                  "reviewers")))
 
-    decided = URIRef(f'{baseURI}/decided')
-    graph.add((decided, RDFS.domain, Review))
-    graph.add((decided, RDFS.range, Decision))
+    graph.add((KG.reviewed, RDFS.domain, KG.Reviewer))
+    graph.add((KG.reviewed, RDFS.range, KG.Submission))
+    graph.add((KG.reviewed, RDFS.label, Literal("reviewed")))
 
-    when = URIRef(f'{baseURI}/when')
-    graph.add((when, RDFS.domain, Submission))
-    graph.add((when, RDFS.range, Year))
+    graph.add((KG.affiliatedTo, RDFS.domain, KG.Author))
+    graph.add((KG.affiliatedTo, RDFS.range, KG.Organization))
+    graph.add((KG.affiliatedTo, RDFS.label, Literal("affiliatedTo")))
 
-    chairs = URIRef(f'{baseURI}/chairs')
-    graph.add((chairs, RDFS.domain, Chair))
-    graph.add((chairs, RDFS.range, Conference))
+    graph.add((KG.partOf, RDFS.domain, KG.Submission))
+    graph.add((KG.partOf, RDFS.range, KG.Review))
+    graph.add((KG.partOf, RDFS.label, Literal("partOf")))
 
-    edits = URIRef(f'{baseURI}/edits')
-    graph.add((edits, RDFS.domain, Editor))
-    graph.add((edits, RDFS.range, Journal))
+    graph.add((KG.commented, RDFS.domain, KG.Review))
+    graph.add((KG.commented, RDFS.range, XSD.string))
+    graph.add((KG.commented, RDFS.label, Literal("commented")))
+    graph.add((KG.commented, RDFS.comment, Literal("The string provides all the commentary available as part of the "
+                                                   "review")))
+
+    graph.add((KG.decided, RDFS.domain, KG.Review))
+    graph.add((KG.decided, RDFS.range, XSD.boolean))
+    graph.add((KG.decided, RDFS.label, Literal("decided")))
+    graph.add((KG.decided, RDFS.comment, Literal("The decision is a boolean where True means the paper is accepted "
+                                                 "and False means the paper is rejected" )))
+
+    graph.add((KG.chairs, RDFS.domain, KG.Chair))
+    graph.add((KG.chairs, RDFS.range, KG.Conference))
+    graph.add((KG.chairs, RDFS.label, Literal("chairs")))
+
+    graph.add((KG.edits, RDFS.domain, KG.Editor))
+    graph.add((KG.edits, RDFS.range, KG.Journal))
+    graph.add((KG.edits, RDFS.label, Literal("edits")))
 
 
 def output_graph_to_turtle(graph_name, filename):
@@ -166,6 +193,7 @@ baseURI = "http://example.org"
 graph = Graph()
 create_TBOX()
 output_graph_to_turtle(graph, "TBOX")
+
 
 
 
