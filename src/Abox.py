9942222fd09fd3110dbb32c7e6408dc815212799
abroniewski <abroniewski@gmail.com>
2022-05-21 12:36:18 +0200

    updated to current version

--- src/Abox.py
+++ src/Abox.py
@@ -41,69 +41,69 @@ Author_Papers = Author.join(Author_by.set_index('idA'), on="idA").join(Papers.se
 # In[184]:
 
 
-Author_Papers.head()
-
-
-# In[130]:
-
-
-Keyword_Papers = Keywords.set_index('id').join(Keyword_paper.set_index('idK')).set_index('idP').join(Papers.set_index('id')).reset_index()
-
-
-# In[185]:
-
-
-Keyword_Papers.head()
-
-
-# In[191]:
-
-
-Journal_Keyword = Journals.set_index('id').join(JVolumes.set_index('idJ')).set_index('id').join(Keyword_Papers.set_index('idEidV')).reset_index()
-
-
-# In[192]:
-
-
-Journal_Keyword.head()
-
-
-# In[188]:
-
-
-Paper_volume = Conferences.set_index('id').join(CEditions.set_index('idC')).set_index('id').join(Keyword_Papers.set_index('idEidV')).reset_index()
-
-
-# In[189]:
-
-
-Paper_volume.head()
-
-
-# In[196]:
-
-Author.rename(columns={"id": "idA"}, inplace=True)
-Review_Papers = Reviews.set_index('idA').join(Author.set_index('id')).set_index('idP').join(Papers.set_index('id')).reset_index()
-
-
-# In[197]:
-
-
-Review_Papers.head()
-
-
-# In[198]:
-
-
-Editor_Chair = Review_Papers.merge(Journal_Keyword, left_on='title', right_on='title',
-                                   how='left').merge(Paper_volume, left_on='title', 
-                                                     right_on='title',how='left')
-
-
-# In[199]:
-
-
-Editor_Chair.head()
+# Author_Papers.head()
+#
+#
+# # In[130]:
+#
+#
+# Keyword_Papers = Keywords.set_index('id').join(Keyword_paper.set_index('idK')).set_index('idP').join(Papers.set_index('id')).reset_index()
+#
+#
+# # In[185]:
+#
+#
+# Keyword_Papers.head()
+#
+#
+# # In[191]:
+#
+#
+# Journal_Keyword = Journals.set_index('id').join(JVolumes.set_index('idJ')).set_index('id').join(Keyword_Papers.set_index('idEidV')).reset_index()
+#
+#
+# # In[192]:
+#
+#
+# Journal_Keyword.head()
+#
+#
+# # In[188]:
+#
+#
+# Paper_volume = Conferences.set_index('id').join(CEditions.set_index('idC')).set_index('id').join(Keyword_Papers.set_index('idEidV')).reset_index()
+#
+#
+# # In[189]:
+#
+#
+# Paper_volume.head()
+#
+#
+# # In[196]:
+#
+#
+# # Review_Papers = Reviews.set_index('idA').join(Author.set_index('id')).set_index('idP').join(Papers.set_index('id')).reset_index()
+#
+#
+# # In[197]:
+#
+#
+# # Review_Papers.head()
+#
+#
+# # In[198]:
+#
+#
+# Editor_Chair = Review_Papers.merge(Journal_Keyword, left_on='title', right_on='title',
+#                                    how='left').merge(Paper_volume, left_on='title',
+#                                                      right_on='title',how='left')
+#
+#
+# # In[199]:
+#
+#
+# Editor_Chair.head()
 
 
 # In[203]:
@@ -121,50 +121,50 @@ for k in range(len(Author_Papers['name'])):
     g.add((Author_Papers['idA'][k], KG.name, Literal(Author_Papers['name'][k])))  # adds human-readable string
 
 
-g.add((KG.Paper, KG.PaperRelatedTo, KG.SubjectArea))
-for k in range(len(Keyword_Papers['keyword'])):
-    g.add((Literal(Keyword_Papers['title'][k]), KG.PaperRelatedTo, Literal(Keyword_Papers['keyword'][k])))
-
-g.add((KG.Journal, KG.JournalRelatedTo, KG.SubjectArea))
-for k in range(len(Journal_Keyword['keyword'])):
-    g.add((Literal(Journal_Keyword['journal'][k]), KG.JournalRelatedTo, Literal(Journal_Keyword['keyword'][k])))
-
-g.add((KG.Conference, KG.ConferenceRelatedTo, KG.SubjectArea))
-for k in range(len(Paper_volume['keyword'])):
-    g.add((Literal(Paper_volume['conference'][k]), KG.ConferenceRelatedTo, Literal(Paper_volume['keyword'][k])))
-
-g.add((KG.Journal, KG.published, KG.Volume))
-for k in range(len(Journal_Keyword['journal'])):
-    g.add((Literal(Journal_Keyword['journal'][k]), KG.published, Literal(Journal_Keyword['volume'][k])))
-
-g.add((KG.Conference, KG.published, KG.Proceeding))
-for k in range(len(Paper_volume['conference'])):
-    g.add((Literal(Paper_volume['conference'][k]), KG.published, Literal(Paper_volume['date'][k])))
-
-g.add((KG.Reviewer, KG.decided, KG.Decision))
-for k in range(len(Review_Papers['name'])):
-    g.add((Literal(Review_Papers['name'][k]), KG.decided, Literal(Review_Papers['decision'][k])))
-
-
-g.add((KG.Conference, KG.handledBy, KG.Chair))
-g.add((KG.Journal, KG.handledBy, KG.Editor))
-for k in range(len(Editor_Chair['conference'])):
-    if Editor_Chair['confjournal'][k] == 'Conference':
-        g.add((Literal(Editor_Chair['conference'][k]), KG.handledBy, Literal(Editor_Chair['name'][k])))
-    else:
-        g.add((Literal(Editor_Chair['journal'][k]), KG.handledBy, Literal(Editor_Chair['name'][k])))
-        
-#Not resonable       
-g.add((KG.Chair, KG.assigned, KG.Reviewer))
-g.add((KG.Editor, KG.assigned, KG.Reviewer))
-for k in range(len(Editor_Chair['conference'])):
-    if Editor_Chair['confjournal'][k] == 'Conference':
-        g.add((Literal(Editor_Chair['name'][k]), KG.assigned, Literal(Editor_Chair['name'][k])))
-    else:
-        g.add((Literal(Editor_Chair['name'][k]), KG.assigned, Literal(Editor_Chair['name'][k])))
-        
-g.add((KG.Reviewer, KG.reviewed, KG.Submission))
-g.add((Literal(Editor_Chair['name'][k]), KG.reviewed, Literal(int(Editor_Chair['date_y'][k]))))
+# g.add((KG.Paper, KG.PaperRelatedTo, KG.SubjectArea))
+# for k in range(len(Keyword_Papers['keyword'])):
+#     g.add((Literal(Keyword_Papers['title'][k]), KG.PaperRelatedTo, Literal(Keyword_Papers['keyword'][k])))
+#
+# g.add((KG.Journal, KG.JournalRelatedTo, KG.SubjectArea))
+# for k in range(len(Journal_Keyword['keyword'])):
+#     g.add((Literal(Journal_Keyword['journal'][k]), KG.JournalRelatedTo, Literal(Journal_Keyword['keyword'][k])))
+#
+# g.add((KG.Conference, KG.ConferenceRelatedTo, KG.SubjectArea))
+# for k in range(len(Paper_volume['keyword'])):
+#     g.add((Literal(Paper_volume['conference'][k]), KG.ConferenceRelatedTo, Literal(Paper_volume['keyword'][k])))
+#
+# g.add((KG.Journal, KG.published, KG.Volume))
+# for k in range(len(Journal_Keyword['journal'])):
+#     g.add((Literal(Journal_Keyword['journal'][k]), KG.published, Literal(Journal_Keyword['volume'][k])))
+#
+# g.add((KG.Conference, KG.published, KG.Proceeding))
+# for k in range(len(Paper_volume['conference'])):
+#     g.add((Literal(Paper_volume['conference'][k]), KG.published, Literal(Paper_volume['date'][k])))
+#
+# g.add((KG.Reviewer, KG.decided, KG.Decision))
+# for k in range(len(Review_Papers['name'])):
+#     g.add((Literal(Review_Papers['name'][k]), KG.decided, Literal(Review_Papers['decision'][k])))
+#
+#
+# g.add((KG.Conference, KG.handledBy, KG.Chair))
+# g.add((KG.Journal, KG.handledBy, KG.Editor))
+# for k in range(len(Editor_Chair['conference'])):
+#     if Editor_Chair['confjournal'][k] == 'Conference':
+#         g.add((Literal(Editor_Chair['conference'][k]), KG.handledBy, Literal(Editor_Chair['name'][k])))
+#     else:
+#         g.add((Literal(Editor_Chair['journal'][k]), KG.handledBy, Literal(Editor_Chair['name'][k])))
+#
+# #Not resonable
+# g.add((KG.Chair, KG.assigned, KG.Reviewer))
+# g.add((KG.Editor, KG.assigned, KG.Reviewer))
+# for k in range(len(Editor_Chair['conference'])):
+#     if Editor_Chair['confjournal'][k] == 'Conference':
+#         g.add((Literal(Editor_Chair['name'][k]), KG.assigned, Literal(Editor_Chair['name'][k])))
+#     else:
+#         g.add((Literal(Editor_Chair['name'][k]), KG.assigned, Literal(Editor_Chair['name'][k])))
+#
+# g.add((KG.Reviewer, KG.reviewed, KG.Submission))
+# g.add((Literal(Editor_Chair['name'][k]), KG.reviewed, Literal(int(Editor_Chair['date_y'][k]))))
 
 
 # In[204]:
